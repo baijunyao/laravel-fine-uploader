0c1318151be07672d5f5b91defcd6a86916d6efe
baijunyao <baijunyao@baijunyao.com>
2018-03-08 16:48:46 +0800

    :sparkles:上传功能完成

--- src/controllers/FineUploaderController.php
+++ src/controllers/FineUploaderController.php
@@ -3,6 +3,7 @@
 namespace Baijunyao\LaravelFineUploader\Controllers;
 
 use File;
+use Ramsey\Uuid\Uuid;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Baijunyao\LaravelFineUploader\Models\FineUploaderFile;
@@ -35,4 +36,42 @@ class FineUploaderController extends Controller
         }
         return response()->json($data);
     }
+
+    /**
+     * 上传文件
+     *
+     * @param Request          $request
+     * @param FineUploaderFile $fineUploaderFileModel
+     *
+     * @return mixed
+     */
+    public function upload(Request $request, FineUploaderFile $fineUploaderFileModel)
+    {
+        // 判断请求中是否包含name=file的上传文件
+        if (! $request->hasFile('file')) {
+            return response_json(401, '没有要上传的文件');
+        }
+        $path = $request->input('path', '');
+        $path = empty($path) ? config('fineUploader.default_path') : 'uploads/'.$path;
+        // 上传文件
+        $result = upload('file', $path);
+        if ($result['status_code'] !== 200) {
+            return response_json(500, '文件上传失败');
+        }
+        $id = [];
+        foreach ($result['data'] as $v) {
+            $fileData = [
+                'id' => Uuid::uuid4(),
+                'name' => $v['name'],
+                'path' => $v['path']
+            ];
+            $id[] = $fineUploaderFileModel->create($fileData);
+        }
+        $data = [
+            'success' => true,
+            'uuid' => implode(',', $id)
+        ];
+        return response_json(200, $data);
+    }
+
 }
--- src/routes/web.php
+++ src/routes/web.php
@@ -5,4 +5,6 @@
 Route::prefix('fineUploader')->namespace('Baijunyao\LaravelFineUploader\Controllers')->group(function () {
     // 文件详情
     Route::get('detail', 'FineUploaderController@detail');
+    // 上传
+    Route::post('upload', 'FineUploaderController@upload');
 });
