09d239dd178efa70b77397ae47e752a22c88ac1a
baijunyao <baijunyao@baijunyao.com>
2018-03-21 17:00:01 +0800

    :bug:统一格式

--- src/resources/views/script.blade.php
+++ src/resources/views/script.blade.php
@@ -1,5 +1,5 @@
 <script>
-    var {{camel_case($element)}}Obj = new qq.FineUploader({
+    var {{ camel_case($element) }}Obj = new qq.FineUploader({
         element: document.getElementById("{{ $element }}"),
         template: 'qq-template-manual-trigger',
         request: {
@@ -23,11 +23,11 @@
                 $('#{{ $element }}').append(inputStr);
 
                 // 设置删除时候的url和参数
-                {{camel_case($element)}}Obj.setDeleteFileEndpoint('{{ url('fineUploader/destroy') }}');
+                {{ camel_case($element) }}Obj.setDeleteFileEndpoint('{{ url('fineUploader/destroy') }}');
                 var deleteParame = {
                     'id' : responseJSON.uuid
                 }
-                {{camel_case($element)}}Obj.setDeleteFileParams(deleteParame, id);
+                {{ camel_case($element) }}Obj.setDeleteFileParams(deleteParame, id);
             },
 
             // 删除文件后的回调函数
@@ -46,11 +46,11 @@
                     console.log(val);
                     $('.qq-file-info').eq(index).find('.js-download').attr('href', downloadUrl+'?id='+val.uuid);
                     $('#{{ $element }}').append(inputStr);
-                    {{camel_case($element)}}Obj.setDeleteFileEndpoint('{{ url('fineUploader/destroy') }}');
+                    {{ camel_case($element) }}Obj.setDeleteFileEndpoint('{{ url('fineUploader/destroy') }}');
                     var deleteParame = {
                         'id' : val.uuid
                     }
-                    {{camel_case($element)}}Obj.setDeleteFileParams(deleteParame, index);
+                    {{ camel_case($element) }}Obj.setDeleteFileParams(deleteParame, index);
                 })
             },
             addInitialFiles: function (file) {
